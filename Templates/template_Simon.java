fae06a305d1e543db8e732a035890b547cf868d5
SimonWameling <44893216+SimonWameling@users.noreply.github.com>
2024-02-20 19:28:35 +0100

    Added Template

---
+++ Templates/template_Simon.java
@@ -0,0 +1,15 @@
+/**
+ * Dies ist ein Template zur Erstellung von Java-Code.
+ * 
+ * @author Simon Wameling
+ * @version openjdk version "17.0.10" 2024-01-16
+ */
+
+// Imports
+
+
+public class template {	
+	public static void main( String[] args ) {
+		System.out.println("Moin!");
+	}
+}
